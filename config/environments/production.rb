49210bfcafd76aa4b0febf3bd94e857e15f747d3
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-09-02 17:29:48 +0900

    rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://dry-retreat-16041.herokuapp.com'
+  host = 'tutorial-test10.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
